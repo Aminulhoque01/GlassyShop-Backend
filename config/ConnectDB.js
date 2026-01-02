adf81fea4b3b55b814deb8e091a5e039cf5fd152
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-02 22:37:00 +0600

    server is running

--- config/ConnectDB.js
+++ config/ConnectDB.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
 
 if(!process.env.MONGODB_URL){
     throw new Error("Please provide MONGODB_URL in the .env file")
--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import helmet from "helmet";
+import connectDB from "./config/ConnectDB.js";
 
+dotenv.config();
 
+const app = express();
 
-
-const app= express();
 app.use(cors());
-app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan());
-app.use(helmet({
-    crossOriginResourcePolicy:false
-}));
+app.use(morgan("dev"));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running " + process.env.PORT });
+});
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port", process.env.PORT);
+    });
+  } catch (error) {
+    console.error("Server startup error:", error);
+  }
+};
 
-app.get("/",(request, response)=>{
-    response.json({message:"Server is running" + process.env.PORT})
-})
+startServer();
