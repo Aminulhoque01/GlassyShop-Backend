e7e1fcbf5e715419ae63fb8eec5cd88fcb43482e
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-02 21:37:35 +0600

    cors express parser morgan import

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+
+
+
+
+const app= express();
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(helmet({
+    crossOriginResourcePolicy:false
+}))
