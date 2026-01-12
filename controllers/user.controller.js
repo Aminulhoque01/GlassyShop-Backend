d9eb6b689a89fb0a8a40c3ee25dcb8db2f897af0
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-12 21:26:17 +0600

    create user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,67 +1,66 @@
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { sendEmail } from "../config/emailService.js";
 import VerificationEmail from "../helper/sendVerificationEmail.js";
 import UserModel from "../models/user.model.js";
-import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 
 export async function registerUserController(request, response) {
   try {
-    let user;
     const { name, email, password } = request.body;
 
     if (!name || !email || !password) {
       return response.status(400).json({
-        message: "provide email, name, password",
+        message: "Provide name, email, password",
         error: true,
         success: false,
       });
     }
 
-   
-    if (user) {
-      return response.json({
-        message: "User already Register with this email",
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return response.status(400).json({
+        message: "User already registered with this email",
         error: true,
         success: false,
       });
     }
+
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
 
     const salt = await bcryptjs.genSalt(10);
     const hashPassword = await bcryptjs.hash(password, salt);
 
-    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
-
-   
-    user = await UserModel.findOne({
-      name: name,
-      email: email,
+    const user = await UserModel.create({
+      name,
+      email,
       password: hashPassword,
       otp: verifyCode,
-      otpExpires: Date.now() + 600000,
+      otpExpires: Date.now() + 10 * 60 * 1000, // 10 minutes
     });
 
-    await user.save();
-
-    //send verifyCation email
-    const verifyEmail = await sendEmail({
+    await sendEmail({
       sendTo: email,
       subject: "Verify email from E-commerce App",
-      text:"",
+      text: "",
       html: VerificationEmail(name, verifyCode),
     });
-    
-    token=JsonWebTokenError.sign({email:user.email, id:user._id},
-      process.env.JSON_WEB_TOKEN_SECRET_KEY
+
+    const token = jwt.sign(
+      { email: user.email, id: user._id },
+      process.env.JSON_WEB_TOKEN_SECRET_KEY,
+      { expiresIn: "7d" }
     );
 
-    return res.status(200).json({
-      success:true,
-      message:"User register successfully ! Please verify your email",
-      token:token,
-    })
+    return response.status(201).json({
+      success: true,
+      error: false,
+      message: "User registered successfully! Please verify your email.",
+      token,
+    });
 
   } catch (error) {
     return response.status(500).json({
-      message: error.message || error,
+      message: error.message,
       error: true,
       success: false,
     });
