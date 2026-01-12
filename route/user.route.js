5faa39eaaad8ff314b4dcc35e710e6cda4e84080
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-12 21:58:30 +0600

    create user route

---
+++ route/user.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { registerUserController } from "../controllers/user.controller";
+
+
+const userRouter= Router();
+
+userRouter.post('/register', registerUserController);
+
+export default userRouter;
