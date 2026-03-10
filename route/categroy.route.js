87bc35a1dc1ae25174eedfb08e48d4a978c12562
Aminulhoque01 <aminuhoque53@gmail.com>
2026-03-10 22:43:37 +0600

    getAllcategory route

--- route/categroy.route.js
+++ route/categroy.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { categoryImageController, createCategory } from "../controllers/category.controller.js";
+import { categoryImageController, createCategory, getAllCategory } from "../controllers/category.controller.js";
 
 const categoryRouter = Router();
 
 categoryRouter.post("/image-upload", categoryImageController);
 categoryRouter.post("/create", createCategory);
+categoryRouter.post("/all", getAllCategory);
 
 export default categoryRouter;
