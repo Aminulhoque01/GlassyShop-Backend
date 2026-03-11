dd9350984c95eb835023507a06636037d02bd7d6
Aminulhoque01 <aminuhoque53@gmail.com>
2026-03-11 22:17:37 +0600

    route

--- route/categroy.route.js
+++ route/categroy.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { categoryImageController, createCategory, getAllCategory } from "../controllers/category.controller.js";
+import auth from "../middleware/auth.js";
 
 const categoryRouter = Router();
 
 categoryRouter.post("/image-upload", categoryImageController);
 categoryRouter.post("/create", createCategory);
-categoryRouter.post("/all", getAllCategory);
+categoryRouter.post("/all", auth, getAllCategory);
 
 export default categoryRouter;
