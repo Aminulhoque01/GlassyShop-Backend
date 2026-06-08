90530ceacfaf612da3d08defb7c1d6323f4cd30e
Aminulhoque01 <aminuhoque53@gmail.com>
2026-06-08 11:24:04 +0600

    updateQty

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,83 +2,116 @@ import { request, response } from "express";
 import CartProductModel from "../models/cartProduct.model.js";
 import UserModel from "../models/user.model.js";
 
+export const addToCartItemController = async (request, response) => {
+  try {
+    const userId = request.userId;
+    const { productId } = request.body;
 
+    if (!productId) {
+      return response.status(402).json({
+        message: "Provide ProductId",
+        error: true,
+        success: false,
+      });
+    }
 
-export const addToCartItemController= async(request, response)=>{
-    try {
-        const userId=request.userId;
-        const {productId}= request.body;
+    const checkItemCart = await CartProductModel.findOne({
+      userId: userId,
+      productId: productId,
+    });
 
-        if(!productId){
-            return response.status(402).json({
-                message:"Provide ProductId",
-                error:true,
-                success:false
-            })
-        }
-        
- 
+    if (checkItemCart) {
+      return response.status(400).json({
+        message: "Item already in cart",
+      });
+    }
 
-        const checkItemCart = await CartProductModel.findOne({
-            userId: userId,
-            productId: productId
-        })
-      
-        if(checkItemCart){
-            return response.status(400).json({
-                message:"Item already in cart"
-            })
-        }
+    const cartItem = new CartProductModel({
+      quantity: 1,
+      userId: userId,
+      productId: productId,
+    });
 
-        const cartItem = new CartProductModel({
-            quantity:1,
-            userId: userId,
-            productId: productId
-        })
+    const save = await cartItem.save();
 
-        const save = await cartItem.save();
+    const updateCartUser = await UserModel.updateOne(
+      { _id: userId },
+      {
+        $push: {
+          shopping_cart: productId,
+        },
+      },
+    );
 
-        const updateCartUser = await UserModel.updateOne({_id:userId},{
-            $push:{
-                shopping_cart: productId
-            }
-        });
-
-        return response.json({
-            data:save,
-            message:"Item add successfully",
-            error:false,
-            success:true,
-        })
-
-    } catch (error) {
+    return response.json({
+      data: save,
+      message: "Item add successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
     return response.status(500).json({
       message: error.message,
       error: true,
       success: false,
     });
-    }
-}
+  }
+};
 
+export const getCartItemController = async (request, response) => {
+  try {
+    const userId = request.userId;
+    const cartItem = await CartProductModel.find({
+      userId: userId,
+    }).populate("productId");
 
-export const getCartItemController= async(request, response)=>{
-    try {
-        const userId= request.userId;
-        const cartItem = await CartProductModel.find({
-            userId:userId
-
-        }).populate("productId")
-
-        return response.status(200).json({
-            data:cartItem,
-            error:false,
-            success:true,
-        })
-    } catch (error) {
+    return response.status(200).json({
+      data: cartItem,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
     return response.status(500).json({
       message: error.message,
       error: true,
       success: false,
     });
+  }
+};
+
+export const updateCartItemQtyController = async (request, response) => {
+  try {
+    const userId= request.userId,
+    const{_id,qty}=request.body,
+
+    if(!_id || !qty){
+        return response.status(400).json({
+            message:"provide _id, qty"
+        })
     }
-}
+
+    const updateCartItem= await CartProductModel.updateOne(
+        {
+            _id:_id,
+            userId:userId,
+        },
+        {
+            quantity:qty
+        }
+    )
+
+    return response.status(200).json({
+        message:"Update cart",
+        success:true,
+        error: false,
+        data: updateCartItem
+    })
+    
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message,
+      error: true,
+      success: false,
+    });
+  }
+};
