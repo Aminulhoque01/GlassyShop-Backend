8a26aa6070b9267f80a82846b4316f44dfe661b0
Aminulhoque01 <aminuhoque53@gmail.com>
2026-06-08 17:02:30 +0600

    delete item controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -81,31 +81,70 @@ export const getCartItemController = async (request, response) => {
 
 export const updateCartItemQtyController = async (request, response) => {
   try {
-    const userId= request.userId 
-    const {_id,qty} =request.body 
+    const userId = request.userId;
+    const { _id, qty } = request.body;
 
-    if(!_id || !qty){
-        return response.status(400).json({
-            message:"provide _id, qty"
-        })
+    if (!_id || !qty) {
+      return response.status(400).json({
+        message: "provide _id, qty",
+      });
     }
 
-    const updateCartItem= await CartProductModel.updateOne(
-        {
-            _id:_id,
-            userId:userId,
-        },
-        {
-            quantity:qty
-        }
-    )
+    const updateCartItem = await CartProductModel.updateOne(
+      {
+        _id: _id,
+        userId: userId,
+      },
+      {
+        quantity: qty,
+      },
+    );
 
     return response.status(200).json({
-        message:"Update cart",
-        success:true,
-        error: false,
-        data: updateCartItem
-    })
+      message: "Update cart",
+      success: true,
+      error: false,
+      data: updateCartItem,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+export const deleteItemQtyController = async (request, response) => {
+  try {
+   const userId= request.userId
+   const {_id} = request.body
+   
+   if(!_id){
+        return response.status(400).json({
+            message:"Provide _id",
+            error:true,
+            success:false,
+        })
+   }
+   
+   const deleteCartItem= await CartProductModel.deleteOne({_id:_id, userId:userId})
+    
+    if(!deleteCartItem){
+     return response.status(404).json({
+      message:"Item remove",
+      error:true,
+      success:false,
+       
+     })
+    }
+  
+  return response.json({
+   message:"Item remove successfully",
+   error:false,
+   success:true,
+   data:deleteCartItem
+  })
 
   } catch (error) {
     return response.status(500).json({
