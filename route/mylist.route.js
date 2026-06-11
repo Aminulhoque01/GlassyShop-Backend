c32f4753ceb9cc9ad4017873966dde9b68beb6d2
Aminulhoque01 <aminuhoque53@gmail.com>
2026-06-11 12:04:18 +0600

    add to cart

--- route/mylist.route.js
+++ route/mylist.route.js
@@ -3,10 +3,10 @@ import auth from "../middleware/auth.js";
 import { addToMyListController, deleteMyListController, getMyListController } from "../controllers/myList.controller.js";
 
 
- const myListRoute= Router();
+const myListRoute= Router();
 
- myListRoute.post("/add-my-list", auth, addToMyListController)
- myListRoute.get("/get-my-list", auth, getMyListController);
- myListRoute.delete("/my-list/:productId",auth, deleteMyListController);
+myListRoute.post("/add-my-list", auth, addToMyListController)
+myListRoute.get("/get-my-list", auth, getMyListController);
+myListRoute.delete("/my-list/:productId",auth, deleteMyListController);
 
  export default myListRoute;
