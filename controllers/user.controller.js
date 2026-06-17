b27f741c7a5cc289955d8356824a7affe7c2f863
Aminulhoque01 <aminuhoque53@gmail.com>
2026-06-17 16:53:36 +0600

    token updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -61,7 +61,7 @@ export async function registerUserController(req, res) {
     );
 
     const token = jwt.sign(
-      { email: user.email, id: user._id },
+      { email:user.email, id:user._id },
       process.env.JSON_WEB_TOKEN_SECRET_KEY,
       { expiresIn: "7d" },
     );
