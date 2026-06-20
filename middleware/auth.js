54abaf2834a6108ea12dddf4edf916e2bb544b03
Aminulhoque01 <aminuhoque53@gmail.com>
2026-06-20 11:39:38 +0600

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken"
 
 const auth= async(request, response, next)=>{
     try {
-        var token =request.cookies?.accessToken || request.headers?.authorization?.split(" ")[1];
-        if(!token){
-            token=request.query.token
-        }
+        const token =request.cookies?.accessToken || request.headers?.authorization?.split(" ")[1];
+        // if(!token){
+        //     token=request.query.token
+        // }
        
 
         if(!token){
