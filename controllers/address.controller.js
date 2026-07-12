01dabbe17da08f1d2174eb46722067ff0460dd34
Aminulhoque01 <aminuhoque53@gmail.com>
2026-07-12 22:31:27 +0600

    address controller

--- controllers/address.controller.js
+++ controllers/address.controller.js
@@ -1,14 +1,38 @@
 import { request, response } from "express";
 
+export const addAddressController = async (request, response) => {
+  try {
+    const {
+      address_line,
+      city,
+      state,
+      pinCode,
+      country,
+      mobile,
+      status,
+      userId,
+    } = request.body;
 
-export const addAddressController= async(request, response)=>{
-    try {
-        
-    } catch (error) {
-        return response.json({
-              message: "address not successfully",
-              success: true,
-              error: false,
-            });
+    if (
+      address_line ||
+      city ||
+      state ||
+      pinCode ||
+      country ||
+      mobile ||
+      userId
+    ) {
+      return response.status(500).json({
+      message:"Please provide all the fields",
+      error: true,
+      success: false,
+    });
     }
-}
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message,
+      error: true,
+      success: false,
+    });
+  }
+};
