50c17dc358cc79b8547d1b8e3c2cb2aa0b691851
Aminulhoque01 <aminuhoque53@gmail.com>
2026-07-19 23:07:33 +0600

    address route

--- route/address.route.js
+++ route/address.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { addAddressController } from "../controllers/address.controller.js";
+import { addAddressController, getAddress } from "../controllers/address.controller.js";
 
  const addressRoute= Router();
 
  addressRoute.post("/add", auth,addAddressController);
+ addressRoute.get("/", auth,getAddress);
 
  export default addressRoute;
 
